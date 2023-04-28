e5525347258d810a06e32c052e645f29c333306b
KevinXZX <72686328+KevinXZX@users.noreply.github.com>
2023-04-28 19:53:01 +0100

    Add workflow

--- src/main/java/com/movierating/repo/RatingRepo.java
+++ src/main/java/com/movierating/repo/RatingRepo.java
@@ -9,21 +9,21 @@ import java.util.List;
 @Repository
 public interface RatingRepo extends JpaRepository<MovieRating, Integer> {
     /**
-     * @param movie_id
-     * @param user_id
+     * @param movie_id id of the movie
+     * @param user_id  id of the user
      * @return List of MovieRatings that have the movie_id and user_id
      */
     List<MovieRating> findByMovieIdAndUserId(String movie_id, int user_id);
 
     /**
-     * @param user_id
+     * @param user_id id of the user
      * @return List of MovieRatings that have the user_id
      */
     List<MovieRating> findByUserId(int user_id);
 
     /**
-     * @param movie_id
-     * @param user_id
+     * @param movie_id id of the movie
+     * @param user_id  id of the user
      * @return number of ratings deleted
      */
     long deleteAllByMovieIdAndUserId(String movie_id, int user_id);
